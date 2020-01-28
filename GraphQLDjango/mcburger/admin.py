3a1e04e277767cf246d459e98a79516e240441bd
Cristian Dominguez <crisdomgo@gmail.com>
2020-01-28 18:21:26 +0100

    Register models in admin

--- GraphQLDjango/mcburger/admin.py
+++ GraphQLDjango/mcburger/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ingredient, Product, Category
+
+admin.site.register(Ingredient)
+admin.site.register(Product)
+admin.site.register(Category)
