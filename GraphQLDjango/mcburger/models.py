6c6faf5082c1c54eacb35ab49c7be4225b3d093e
Cristian Dominguez <crisdomgo@gmail.com>
2020-01-28 17:39:09 +0100

    Add Model

--- GraphQLDjango/mcburger/models.py
+++ GraphQLDjango/mcburger/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    extra_price = models.FloatField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    price = models.FloatField()
+    ingredients = models.ManyToManyField(Ingredient)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
